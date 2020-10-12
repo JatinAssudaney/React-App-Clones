63b4a2b4d204eb1f70d7600418452b2a2a89d114
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-13 02:12:15 +0530

    Firebase Connection

--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -3,11 +3,15 @@ import { render } from "react-dom";
 import "normalize.css";
 import App from "./app";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 render(
   <Fragment>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </Fragment>,
   document.getElementById("root")
 );
