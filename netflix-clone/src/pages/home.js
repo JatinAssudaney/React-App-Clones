f8ad8df16caeb08499d7e961f33163bb9eb2f855
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-12 16:24:36 +0530

    Accordion Component Complete

--- netflix-clone/src/pages/home.js
+++ netflix-clone/src/pages/home.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
+import { FaqsContainer } from "../containers/faqs";
 
 export default function Home() {
   return (
     <Fragment>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </Fragment>
   );
