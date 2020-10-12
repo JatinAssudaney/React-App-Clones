6690b4738eb60a532a6a9e9542fa2dc3e1d5ab60
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-12 20:02:39 +0530

    Creating React Router Routes

--- netflix-clone/src/app.js
+++ netflix-clone/src/app.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { Home } from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <Signin />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <Signup />
+        </Route>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
