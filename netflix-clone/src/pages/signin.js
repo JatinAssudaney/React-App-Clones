908955d05f6ec6921cab326eed6b6dff93441f31
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-13 15:05:19 +0530

    SignUp Route Component Complete

--- netflix-clone/src/pages/signin.js
+++ netflix-clone/src/pages/signin.js
@@ -49,7 +49,11 @@ export default function Signin() {
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
+            <Form.Submit
+              disabled={isInvalid}
+              type="submit"
+              data-testid="sign-in"
+            >
               Sign In
             </Form.Submit>
           </Form.Base>
